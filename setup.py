3f6873b874f00dbfc440e855f813030fce107965
kenkov <kenkovtan@gmail.com>
2017-10-09 01:55:27 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+# coding:utf-8
+
+from distutils.core import setup
+
+
+setup(
+    name="dialog_search_engine",
+    packages=["dialog_search_engine"],
+    version="0.0.1",
+    author="kenkov",
+    author_email="kenkovtan@gmail.com",
+    url="http://kenkov.jp",
+)
