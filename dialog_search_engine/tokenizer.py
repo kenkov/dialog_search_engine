15d94c41562e2eb4ae746cb2b2f252c2f49de568
kenkov <kenkovtan@gmail.com>
2017-10-13 08:36:50 +0900

    WordTokenizer の pos に「感動詞」を追加

--- dialog_search_engine/tokenizer.py
+++ dialog_search_engine/tokenizer.py
@@ -8,4 +8,9 @@ from cabocha.tokenizer import CaboChaBasicTokenizer
 class CaboChaContentWordTokenizer(CaboChaBasicTokenizer):
     """CaboCha による内容語トークナイザ"""
     def __init__(self):
-        super().__init__(pos={"動詞", "形容詞", "名詞"})
+        pos = {"動詞",
+               "形容詞",
+               "名詞",
+               "感動詞",
+               }
+        super().__init__(pos=pos)
