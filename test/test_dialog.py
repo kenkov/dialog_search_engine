1a3339b5d3adb4e632245c8eccea4e6bce468eb4
kenkov <kenkovtan@gmail.com>
2018-01-24 03:41:31 +0900

    Dialog, DialogFactory のテストを追加

---
+++ test/test_dialog.py
@@ -0,0 +1,45 @@
+#! /usr/bin/env python
+# coding:utf-8
+
+import unittest
+from dialog_search_engine.dialog import DialogFactory
+from dialog_search_engine.dialog import Dialog
+from dialog_search_engine.document import Document
+
+
+class DocumentFactoryDouble:
+    def build(self, text):
+        return Document(text=text,
+                        tokens=text.split(" "))
+
+
+class TestDialogFactory(unittest.TestCase):
+    def test_build(self):
+        doc_factory = DocumentFactoryDouble()
+        factory = DialogFactory(document_factory=doc_factory)
+        input_text = "明日 は 晴れ"
+        response_text = "ピクニック に 行こう"
+        res = factory.build(input_text,
+                            response_text,
+                            id_=100)
+        ans = Dialog(input_doc=doc_factory.build(input_text),
+                     response_doc=doc_factory.build(response_text),
+                     id_=100)
+        self.assertEqual(res, ans)
+
+
+class TestDialog(unittest.TestCase):
+    def test_encode_decode(self):
+        doc_factory = DocumentFactoryDouble()
+        factory = DialogFactory(document_factory=doc_factory)
+        input_text = "明日 は 晴れ"
+        response_text = "ピクニック に 行こう"
+        dialog = factory.build(input_text, response_text, id_=100)
+
+        encoded_dialog = dialog.encode()
+        self.assertEqual(encoded_dialog,
+                         ("明日 は 晴れ", "明日 は 晴れ",
+                          "ピクニック に 行こう", "ピクニック に 行こう",
+                          100))
+        decoded_dialog = Dialog.decode(*encoded_dialog)
+        self.assertEqual(decoded_dialog, dialog)
