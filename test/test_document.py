ba4b0576af7edfe0530d739767352ca21ae6e135
kenkov <kenkovtan@gmail.com>
2018-01-24 03:40:06 +0900

    Document, DocumentFacotry のテストを追加

--- test/test_document.py
+++ test/test_document.py
@@ -2,7 +2,7 @@
 # coding:utf-8
 
 import unittest
-from dialog_search_engine.document import DocmumentFactory
+from dialog_search_engine.document import DocumentFactory
 from dialog_search_engine.document import Document
 
 
@@ -13,9 +13,20 @@ class TokenizerDouble:
 
 class TestDocumentFactory(unittest.TestCase):
     def test_build(self):
-        factory = DocmumentFactory(tokenizer=TokenizerDouble())
+        factory = DocumentFactory(tokenizer=TokenizerDouble())
         text = "明日は晴れ"
         res = factory.build(text)
         ans = Document(text="明日は晴れ",
                        tokens=["明日", "晴れ"])
         self.assertEqual(res, ans)
+
+
+class TestDocument(unittest.TestCase):
+    def test_encode_decode(self):
+        text = Document(text="明日は晴れ",
+                        tokens=["明日", "晴れ"])
+        encoded_text = text.encode()
+        self.assertEqual(encoded_text,
+                         ("明日は晴れ", "明日 晴れ"))
+        decoded_text = Document.decode(*encoded_text)
+        self.assertEqual(decoded_text, text)
