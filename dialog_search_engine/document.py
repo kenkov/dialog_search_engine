6d8a30d3ed2ead6622d98fd53a75766290798b03
kenkov <kenkovtan@gmail.com>
2018-01-25 20:32:29 +0900

    Document.encoder, Document.decoder を実装

--- dialog_search_engine/document.py
+++ dialog_search_engine/document.py
@@ -17,11 +17,24 @@ class Document:
     def tokens(self):
         return self._tokens
 
+    def encode(self):
+        """データベースに格納する形式に変換する"""
+        return self.text, " ".join(self.tokens)
+
+    @classmethod
+    def decode(cls, text, tokens_str):
+        """データベースの形式からオブジェクトに変換する"""
+        tokens = tokens_str.split(" ")
+        return cls(text, tokens)
+
     def __str__(self):
         return f"Document({self._text}, {self._tokens})"
 
+    def __eq__(self, doc):
+        return self.text == doc.text and self.tokens == doc.tokens
 
-class DocmumentFactory:
+
+class DocumentFactory:
     def __init__(self, tokenizer=CaboChaContentWordTokenizer()):
         self._tokenizer = tokenizer
 
