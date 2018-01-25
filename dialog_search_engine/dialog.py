3ce29de12de9bb742cbc411a39403393a89df324
kenkov <kenkovtan@gmail.com>
2018-01-25 20:32:45 +0900

    DialogFactory を実装

--- dialog_search_engine/dialog.py
+++ dialog_search_engine/dialog.py
@@ -2,28 +2,44 @@
 # coding:utf-8
 
 
-class Dialog:
-    def __init__(self, id_, length, input_, response):
-        self.id = id_
-        self.length = length
-        self.input = input_
-        self.response = response
+from dialog_search_engine.document import Document
+from dialog_search_engine.document import DocumentFactory
 
-    @staticmethod
-    def decode(id_, length, input_, response):
-        return Dialog(id_, length, input_, response)
+
+class Dialog:
+    def __init__(self, input_doc, response_doc, id_):
+        self.input_doc = input_doc
+        self.response_doc = response_doc
+        self.id_ = id_
+
+    @classmethod
+    def decode(cls,
+               input_text,
+               input_tokens_str,
+               response_text,
+               response_tokens_str,
+               id_):
+        input_doc = Document.decode(input_text, input_tokens_str)
+        response_doc = Document.decode(response_text, response_tokens_str)
+        return Dialog(input_doc, response_doc, id_)
 
     def encode(self):
-        return (self.id, self.length, self.input, self.response)
+        return (self.input_doc.encode() +
+                self.response_doc.encode() +
+                (self.id_,))
 
     def __eq__(self, other):
-        return (self.id == other.id and
-                self.length == other.length and
-                self.input == other.input and
-                self.response == other.response)
+        return (self.input_doc == other.input_doc and
+                self.response_doc == other.response_doc,
+                self.id_ == other.id_)
 
-    def __str__(self):
-        return f'Dialog(id={self.id},length="{self.length}",input="{self.input}",response="{self.response}")'
 
-    def __repr__(self):
-        return self.__str__()
+class DialogFactory:
+    def __init__(self,
+                 document_factory=DocumentFactory()):
+        self._doc_factory = document_factory
+
+    def build(self, input_text, response_text, id_=0):
+        input_doc = self._doc_factory.build(input_text)
+        response_doc = self._doc_factory.build(response_text)
+        return Dialog(input_doc, response_doc, id_=id_)
