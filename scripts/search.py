5c232ebcff4a78da533d106e97eec22805b3e3f8
kenkov <kenkovtan@gmail.com>
2018-01-25 20:33:26 +0900

    モジュールのAPI変更に対応

--- scripts/search.py
+++ scripts/search.py
@@ -3,14 +3,16 @@
 
 
 if __name__ == "__main__":
+    from dialog_search_engine.database import Database
     from dialog_search_engine.index_searcher import IndexSearcher
-    from dialog_search_engine.tokenizer import CaboChaContentWordTokenizer
+    from dialog_search_engine.scorer import IdfWeightedJaccardScorer
     import sys
     import datetime
 
     dbname = sys.argv[1]
-    tokenizer = CaboChaContentWordTokenizer()
-    searcher = IndexSearcher(dbname, tokenizer)
+    db = Database(dbname)
+    scorer = IdfWeightedJaccardScorer(db)
+    searcher = IndexSearcher(db=db, scorer=scorer)
 
     for line in sys.stdin:
         query = line.strip("\n")
@@ -21,9 +23,11 @@ if __name__ == "__main__":
             len(res),
             end_at - start_at
         ))
-        for score_dialog in res[:10]:
+        for score_dialog in res[:5]:
             score = score_dialog.score
             dialog = score_dialog.dialog
-            print("{:4f} {} {}".format(score,
-                                       dialog.input,
-                                       dialog.response))
+            print("{:4f}\t{}\t{}\t{}\t{}".format(score,
+                                       dialog.input_doc.text,
+                                       "|".join(dialog.input_doc.tokens),
+                                       dialog.response_doc.text,
+                                       "|".join(dialog.response_doc.tokens)))
